eb0f7a13fe1b95431fad7d866d2385296e4c09e6
Luiz Carneiro <luiz@carneiro.dev>
2025-02-22 11:14:42 +0100

    Article page started

--- src/components/home/TopCategories.tsx
+++ src/components/home/TopCategories.tsx
@@ -1,18 +1,19 @@
-import { categories } from "@/lib/dataPlaceholder";
+// import { categories } from "@/lib/dataPlaceholder";
+import { POSTS } from "@/lib/constants";
 import { Button } from "../ui/button";
 import Link from "next/link";
 
 export default function TopCategories() {
   return (
     <div className="grid grid-cols-[repeat(auto-fill,minmax(100px,1fr))] gap-2">
-      {categories.map((category) => (
+      {POSTS.map((post) => (
         <Button
-          key={category}
+          key={`${post.title}_categories`}
           variant="secondary"
           asChild
           className="hover:scale-110 transition-all"
         >
-          <Link href={`/blog/${category}`}>{category}</Link>
+          <Link href={`${post.href}`}>{post.title}</Link>
         </Button>
       ))}
     </div>
